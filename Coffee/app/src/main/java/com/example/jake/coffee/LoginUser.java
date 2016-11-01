418a15148e8d8c5b6ec8aba086542076564d698b
Jake <C14706715@mydit.ie>
2016-11-01 12:41:03 +0000

    Added features to login but it is bugged

--- Coffee/app/src/main/java/com/example/jake/coffee/LoginUser.java
+++ Coffee/app/src/main/java/com/example/jake/coffee/LoginUser.java
@@ -12,18 +12,8 @@ public class LoginUser extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_login_user);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
