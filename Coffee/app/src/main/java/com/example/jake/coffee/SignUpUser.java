12b488f272f3973a0e54b53f1e31276cf4359509
Jake <C14706715@mydit.ie>
2016-11-01 16:49:26 +0000

    Added button links

--- Coffee/app/src/main/java/com/example/jake/coffee/SignUpUser.java
+++ Coffee/app/src/main/java/com/example/jake/coffee/SignUpUser.java
@@ -1,11 +1,10 @@
 package com.example.jake.coffee;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class SignUpUser extends AppCompatActivity {
 
@@ -14,6 +13,22 @@ public class SignUpUser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_user);
 
+        Button Login = (Button) findViewById(R.id.LoginBtnID);
+        Button SignUp = (Button) findViewById(R.id.SignUpBtnID);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpUser.this, LoginUser.class));
+            }
+        });
+
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpUser.this, start.class));
+            }
+        });
     }
 
 }
