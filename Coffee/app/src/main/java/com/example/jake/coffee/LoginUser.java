0621ab357515fa19aa4f89921904c9426cb1f34d
Jake <C14706715@mydit.ie>
2016-11-01 13:17:29 +0000

    Fixed bug and added start page

--- Coffee/app/src/main/java/com/example/jake/coffee/LoginUser.java
+++ Coffee/app/src/main/java/com/example/jake/coffee/LoginUser.java
@@ -1,11 +1,13 @@
 package com.example.jake.coffee;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginUser extends AppCompatActivity {
 
@@ -13,6 +15,15 @@ public class LoginUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_user);
+
+        Button Login = (Button) findViewById(R.id.LoginBtnID);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginUser.this, start.class));
+            }
+        });
 
     }
 
