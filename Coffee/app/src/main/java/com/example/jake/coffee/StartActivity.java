da39f7e9042cd42b8b3ed549b2446fba090ce599
Jake <C14706715@mydit.ie>
2016-11-03 22:41:08 +0000

    Adding list view to button page

--- Coffee/app/src/main/java/com/example/jake/coffee/StartActivity.java
+++ Coffee/app/src/main/java/com/example/jake/coffee/StartActivity.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class StartActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +20,48 @@ public class StartActivity extends AppCompatActivity {
 
         Button Timer = (Button) findViewById(R.id.TimerBtnID);
 
-        Timer.setOnClickListener(new View.OnClickListener() {
+        Timer.setOnClickListener(new android.view.View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(StartActivity.this, TimerActivity.class));
             }
         });
+
+        String[] myStringArray={
+                "Timer", "Mapping"
+        };
+
+
+        final ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, myStringArray);
+        ListView myList = (ListView) findViewById(R.id.ButtonListView);
+        myList.setAdapter(myAdapter);
+
+        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = null;
+                switch(position){
+                    case 0:
+                        intent = new Intent (StartActivity.this, TimerActivity.class);
+                        break;
+                    case 1:
+                        intent= new Intent (StartActivity.this, LoginUser.class);
+                        break;
+                }
+                startActivity(intent);
+
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
